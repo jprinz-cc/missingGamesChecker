43509fdfafc07d919e291e4314fb2db5597960a3
JP <jp@johnprinz.com>
2023-07-17 11:55:00 -0400

    Working with platform selection;

--- js/missingGamesChecker.js
+++ js/missingGamesChecker.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // Container elements
     const elPlatformSelect = document.querySelector('#platformForm');
     const elCompareForm = document.querySelector("#comparisonForm");
+    const elResultContainer = document.getElementById('resultContainer');
+    const elResultTable = document.getElementById('missingItemsList');
     
     // Controls
     const propertyInput = document.querySelector('#property');
@@ -86,23 +88,48 @@ document.addEventListener("DOMContentLoaded", () => {
         let reader1 = new FileReader();
         reader1.onload = function(e) {
             let xml1 = e.target.result;
+
+            let parser = new DOMParser();
+            let xmlDoc1 = parser.parseFromString(xml1, 'text/xml');
+
+            // Create an array to store the values
+            const valuesArray = [];
+
+            if(xmlDoc1.firstChild.tagName == "LaunchBox" && xmlDoc1.firstChild.firstElementChild.tagName == "Game"){
+                const xpathQuery = `//Game[Platform='${platformSelect.value}']`;
+                const matchingItems = xmlDoc1.evaluate(xpathQuery, xmlDoc1, null, XPathResult.ANY_TYPE, null);                
+
+                // Iterate over the matching items and extract the values
+                let item = matchingItems.iterateNext();
+                while (item) {
+                    console.log("item: ", item);
+                    //const value = item.querySelector('Platform').textContent;
+                    let newItem = {};
+                    newItem.title = item.querySelector('Name').textContent;
+                    newItem.databaseID = item.querySelector('DatabaseID').textContent;
+                    valuesArray.push(newItem);
+                    item = matchingItems.iterateNext();
+                }
+
+                console.log(valuesArray);
+
+            }
     
             let reader2 = new FileReader();
             reader2.onload = function(e) {
                 let xml2 = e.target.result;
     
-                let parser = new DOMParser();
-                let xmlDoc1 = parser.parseFromString(xml1, 'text/xml');
+               
                 let xmlDoc2 = parser.parseFromString(xml2, 'text/xml');
     
-                let items1 = xmlDoc1.getElementsByTagName('Game');
+                //let items1 = xmlDoc1.getElementsByTagName('Game');
                 let items2 = xmlDoc2.getElementsByTagName('Game');
     
                 let missingItems = [];
     
-                for (let i = 0; i < items1.length - 1; i++) {
-                    if(items1[i].getElementsByTagName(property)[0]){
-                        let propertyValue = items1[i].getElementsByTagName(property)[0].textContent;
+                for (let i = 0; i < valuesArray.length - 1; i++) {
+                    if(valuesArray[i].databaseID){
+                        let propertyValue = valuesArray[i].databaseID;
     
                         let found = false;
                         for (let j = 0; j < items2.length; j++) {
@@ -118,7 +145,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         if (!found) {
                             let missingItem = {};
                             missingItem.databaseID = propertyValue;
-                            missingItem.title = items1[i].getElementsByTagName("Name")[0].textContent;
+                            missingItem.title = valuesArray[i].title;
                             missingItems.push(missingItem);
                         }
                     }
@@ -138,7 +165,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function resetForms(){
         elPlatformSelect.style.display = 'none';
         elCompareForm.style.display = 'none';
-        resultContainer.style.display = 'none';
+        elResultContainer.style.display = 'none';
         msglbPlatformFile.innerHTML = "";
         msgPlatformSelect.innerHTML = "";
         platformSelect.selectedIndex = 0;
@@ -154,37 +181,37 @@ document.addEventListener("DOMContentLoaded", () => {
             elCompareForm.style.display = 'none';
         }
     }
+
+    function displayResult(missingItems) {
+
+        elResultTable.innerHTML = '';
+    
+        if (missingItems.length === 0) {
+            elResultTable.innerHTML = '<tr><td>No missing items found.</td></tr>';
+        } else {
+            let header = elResultTable.createTHead();
+            let hRow = header.insertRow(0);
+            let cell1 = hRow.insertCell(0);
+            let cell2 = hRow.insertCell(1);
+            cell1.innerHTML = "Title";
+            cell2.innerHTML = "DatabaseID";
+    
+            for (let i = 0; i < missingItems.length; i++) {
+                let row = elResultTable.insertRow(i+1);
+                let cellTitle = row.insertCell(0);
+                let cellId = row.insertCell(1);
+                cellTitle.innerHTML = missingItems[i].title;
+                cellId.innerHTML = missingItems[i].databaseID;
+    
+            }
+        }
+    
+        elResultContainer.style.display = 'block';
+    }
 });
 
 
 
-function displayResult(missingItems) {
-    let resultContainer = document.getElementById('resultContainer');
-    let resultTable = document.getElementById('missingItemsList');
 
-    resultTable.innerHTML = '';
-
-    if (missingItems.length === 0) {
-        resultTable.innerHTML = '<tr><td>No missing items found.</td></tr>';
-    } else {
-        let header = resultTable.createTHead();
-        let hRow = header.insertRow(0);
-        let cell1 = hRow.insertCell(0);
-        let cell2 = hRow.insertCell(1);
-        cell1.innerHTML = "Title";
-        cell2.innerHTML = "DatabaseID";
-
-        for (let i = 0; i < missingItems.length; i++) {
-            let row = resultTable.insertRow(i+1);
-            let cellTitle = row.insertCell(0);
-            let cellId = row.insertCell(1);
-            cellTitle.innerHTML = missingItems[i].title;
-            cellId.innerHTML = missingItems[i].databaseID;
-
-        }
-    }
-
-    resultContainer.style.display = 'block';
-}
 
 
